9601dbe8546279621dbcbcb4d04978825da8f9d1
Lucas S B Gomes <lucas.sbgomes@gmail.com>
2023-10-25 14:54:02 -0300

    feat: change for npm and add host in app listen

--- src/env/index.ts
+++ src/env/index.ts
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
-  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']).default('pg'),
   DATABASE_URL: z.string().min(1),
   PORT: z.coerce.number().default(3333),
 });
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { app } from './app';
 
 app
   .listen({
+    host: '0.0.0.0',
     port: env.PORT,
   })
   .then(() => {
